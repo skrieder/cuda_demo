afcf7a873343c742c01c233fe63030c88c897243
Scott J. Krieder <skrieder@iit.edu>
2013-10-31 01:21:11 -0500

    adding examples

---
+++ MatMul/Makefile
@@ -0,0 +1,12 @@
+multNoShare : multNoShare.c multNoShare.h
+	cp multNoShare.c multNoShare.cu
+	nvcc -o multNoShare multNoShare.cu
+	rm multNoShare.cu
+
+
+multShare : multShare.c multShare.h
+	cp multShare.c multShare.cu
+	nvcc -o multShare multShare.cu
+	rm multShare.cu
+
+all : multShare multNoShare
---
+++ MatMul/multShare.h
@@ -0,0 +1,27 @@
+/*
+ * multShare.h
+ *
+ * Robert Hochberg
+ * January 24, 2012
+ *
+ * Based nearly entirely on the code from the CUDA C Programming Guide
+ */
+
+#include <stdio.h>
+
+// Matrices are stored in row-major order: 
+// M(row, col) = *(M.elements + row * M.width + col) 
+typedef struct { 
+  int width; 
+  int height; 
+  float* elements; 
+  int stride;
+} Matrix; 
+
+// Thread block size 
+#ifndef BLOCK_SIZE
+#define BLOCK_SIZE 16 
+#endif
+
+__global__ void MatMulKernel(const Matrix, const Matrix, Matrix); 
+
