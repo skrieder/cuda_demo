7f69dc928ea144830b2210f83ef9ece5178377cf
Scott Krieder <skrieder@iit.edu>
2013-10-31 09:30:26 -0500

    adding hello example

---
+++ Hello/hello.cu
@@ -0,0 +1,41 @@
+#include <stdio.h>
+ 
+const int N = 16; 
+const int blocksize = 16; 
+ 
+__global__ 
+void hello(char *ad, int *bd) 
+{
+  int tidx = blockIdx.x;
+  int tidy = blockIdx.y;
+  int tidz = threadIdx.x;
+  printf("TID = <%d,%d,%d>\n", tidx, tidy, tidz);
+}
+ 
+int main()
+{
+  char a[N] = "Hello \0\0\0\0\0\0";
+  int b[N] = {15, 10, 6, 0, -11, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+ 
+  char *ad;
+  int *bd;
+  const int csize = N*sizeof(char);
+  const int isize = N*sizeof(int);
+ 
+  printf("%s", a);
+ 
+  cudaMalloc( (void**)&ad, csize ); 
+  cudaMalloc( (void**)&bd, isize ); 
+  cudaMemcpy( ad, a, csize, cudaMemcpyHostToDevice ); 
+  cudaMemcpy( bd, b, isize, cudaMemcpyHostToDevice ); 
+  
+  dim3 dimGrid( 51, 175 );
+  dim3 dimBlock( 51, 1 );
+  hello<<<dimGrid, dimBlock>>>(ad, bd);
+  cudaMemcpy( a, ad, csize, cudaMemcpyDeviceToHost ); 
+  cudaFree( ad );
+  cudaFree( bd );
+
+  printf("%s\n", a);
+  return EXIT_SUCCESS;
+}
